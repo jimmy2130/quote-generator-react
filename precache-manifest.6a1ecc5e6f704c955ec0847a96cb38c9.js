54b10efc49abd7739b8e4cf0995ae30999c8c439
陳鎮宇 <ccking37@gmail.com>
2020-09-27 12:46:23 +0800

    Updates

---
+++ precache-manifest.6a1ecc5e6f704c955ec0847a96cb38c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9256fc42470e7c04aca9e5339849bb23",
+    "url": "/quote-generator-react/index.html"
+  },
+  {
+    "revision": "350bbafe9cf864ad4c48",
+    "url": "/quote-generator-react/static/css/main.b458dc0f.chunk.css"
+  },
+  {
+    "revision": "22c2870bee1ef406de44",
+    "url": "/quote-generator-react/static/js/2.8b9678b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator-react/static/js/2.8b9678b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350bbafe9cf864ad4c48",
+    "url": "/quote-generator-react/static/js/main.91ae0bd9.chunk.js"
+  },
+  {
+    "revision": "7406f9467e2e698fc253",
+    "url": "/quote-generator-react/static/js/runtime-main.880ec01f.js"
+  }
+]);
